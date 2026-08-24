9bc30ec38312a92234773dfd751a258a91f357e4
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2026-08-24 15:30:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,4 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", None)
 MONGO_URI = os.getenv("MONGO_URI", None)
-
-# നിങ്ങളുടെ ടെലിഗ്രാം അക്കൗണ്ട് ഐഡി (Owner ID) ഇവിടെ Koyeb വഴി നൽകണം
-# ഇത് ബോട്ട് ദുരുപയോഗം ചെയ്യാതിരിക്കാൻ സഹായിക്കും
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
